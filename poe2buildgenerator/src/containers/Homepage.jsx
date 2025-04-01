5b55e79aaf172e754ad23bc2d83155ad9f5bcda6
EnigmaMachine1 <williamstew@outlook.com>
2025-04-01 14:21:54 -0400

    added nonfunctional buttons

--- poe2buildgenerator/src/containers/Homepage.jsx
+++ poe2buildgenerator/src/containers/Homepage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Homepage = () => {
-    return <div>HOMEPAGE</div>
+
+    const handleCharacterList = () => {
+        alert('NOT YET IMPLEMENTED');
+    }
+
+    const handleCharacterGenerator = () => {
+        alert('NOT YET IMPLEMENTED');
+    }
+
+    return (
+        <div className='flex flex-row justify-center items-center h-screen'>
+            <button className='bg-white text-black p-5' onClick={handleCharacterList}>Character List</button>
+            <br/><br/>
+            <button className='bg-white text-black p-5' onClick={handleCharacterGenerator}>Character List</button>
+        </div>
+    );
 };
 
 export default Homepage;
--- poe2buildgenerator/src/index.js
+++ poe2buildgenerator/src/index.js
@@ -6,8 +6,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <body className='bg-black flex text-white'>
-    <Homepage />
+    <body className='bg-black text-white'>
+      <Homepage />
     </body>
   </React.StrictMode>
 );
