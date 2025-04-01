3bda156a15494eb9938643c9384e1f7fd8e397ad
EnigmaMachine1 <williamstew@outlook.com>
2025-04-01 11:00:05 -0400

    added tailwind and homepage component

--- poe2buildgenerator/src/index.js
+++ poe2buildgenerator/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Homepage from './containers/Homepage';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <body className='bg-black flex text-white'>
     <Homepage />
+    </body>
   </React.StrictMode>
 );
